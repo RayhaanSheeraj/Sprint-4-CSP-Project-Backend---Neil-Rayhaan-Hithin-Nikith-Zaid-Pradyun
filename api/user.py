6757584d17ecddf07fbbd6e3717824e658ceeeef
zaidalrefai1 <zaidalrefai5@gmail.com>
2024-12-12 14:37:13 -0800

    user fix

--- api/user.py
+++ api/user.py
@@ -259,9 +259,5 @@ class UserAPI:
 api.add_resource(UserAPI._BULK_CRUD, '/users')
 api.add_resource(UserAPI._CRUD, '/user')
 api.add_resource(UserAPI._Security, '/authenticate')
-<<<<<<< HEAD
 
-=======
-# api.add_resource(UserAPI._Color, '/color')
->>>>>>> 5a332f8f9c5ec40963559a3c4d89a02f37affcff
 
